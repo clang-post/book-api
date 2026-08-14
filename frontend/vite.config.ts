109c4efbc35a00b618b3986e00e4f81fa50b3836
clang-post <collin@pebblepost.com>
2026-08-14 11:41:57 -0400

    Add port forwarding to FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
+    allowedHosts: ['.app.github.dev', '.githubpreview.dev', 'localhost'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
